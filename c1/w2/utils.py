d33183246a7d3bb235a13cdfa91f94fe630d7166
Aaditya Bajgain <adityabajgain@gmail.com>
2026-01-30 00:49:12 -0800

    mnist digit data with nn

--- c1/w2/utils.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-class callback(tf.keras.callbacks.Callback):
-    def end_on_epoch(self, epoch, logs ={}):
-        if (logs.get("Accuracy") >= 0.90):
-            self.model.stop_training = True
